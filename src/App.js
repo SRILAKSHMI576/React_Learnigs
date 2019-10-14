850e20cc19a09e7f9d11e3895eea6daf2412b98c
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2019-10-14 18:51:22 +0530

    Create a component and push component in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import SpaceX from './SpaceX/SpaceX';
 
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello world</h1>
+     <SpaceX />
     </div>
   );
 }
