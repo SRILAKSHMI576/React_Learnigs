8d95d27d679972b87b26d97ca207d9b49e9b3f88
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2019-11-26 16:36:40 +0530

    Apply mouseOver to an element

--- src/UdemyCourse/ClassEx.js
+++ src/UdemyCourse/ClassEx.js
@@ -6,14 +6,46 @@ class ClassEx extends React.Component {
       { name: "sri", age: 60 },
       { name: "laxmi", age: 45 },
       { name: "max", age: 31 }
-    ]
+    ],
+    otherDetail: "something happen"
+  };
+
+  switchNameHandler = () => {
+    // console.log("hii");
+    this.setState({
+      person: [
+        { name: "srilakshmi", age: 60 },
+        { name: "laxmi", age: 45 },
+        { name: "max", age: 35 }
+      ],
+      hover: false
+    });
+  };
+
+  changeColor = () => {
+    this.setState({
+      hover: true
+    });
   };
   render() {
+    const style = {
+      color: this.state.hover ? "red" : undefined
+    };
     return (
       <div>
-        {this.state.person.map(detail => {
+        <button onClick={this.switchNameHandler}>Switching</button> <br />
+        {/* {this.state.person.map(detail => {
           return detail.name;
-        })}
+        })} */}
+        name: {this.state.person[0].name} , age: {this.state.person[0].age}
+        <br />
+        name: {this.state.person[1].name} , age: {this.state.person[1].age}
+        <br />
+        name: {this.state.person[2].name} , age: {this.state.person[2].age}
+        <br />
+        <label onMouseOver={this.changeColor} style={style}>
+          {this.state.otherDetail}
+        </label>
       </div>
     );
   }
